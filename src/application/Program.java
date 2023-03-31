e2c51a23c6a537075c424384bc432948132b4048
Ronery Filho <roneryrffilho@gmail.com>
2023-03-31 14:12:48 -0300

    Added the findById method in SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Finances");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(15, "James", "james@gmail.com", new Date(), 3000.00, obj);
-		System.out.println(seller);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		System.out.println(sellerDao);
+
+		Seller seller = sellerDao.findById(3);
 		
+		System.out.println(seller);
 	}
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -45,6 +45,6 @@ public class Department implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Department : [ID = " + this.id + ", Name = " + this.name + "]";
+		return "Department : [ID " + this.id + ", Name = " + this.name + "]";
 	}
 }
