6ae3bba8b3345f865c871c44fb742cd120f1a95e
Ronery Filho <roneryrffilho@gmail.com>
2023-03-31 11:07:01 -0300

    Added DaoFactory and SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,5 +16,9 @@ public class Program {
 		
 		Seller seller = new Seller(15, "James", "james@gmail.com", new Date(), 3000.00, obj);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println(sellerDao);
+		
 	}
 }
