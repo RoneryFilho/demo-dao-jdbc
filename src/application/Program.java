adb412c7b4860c2b521f21500179e1f2d7663c8f
Ronery Filho <roneryrffilho@gmail.com>
2023-04-03 13:46:13 -0300

    Added the DepartmentDaoJDBC class and its functionalities and added the createDepartmentDao to DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -19,12 +20,11 @@ public class Program {
 		
 		System.out.println("=== TEST 1: seller findById===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		
 		System.out.println("\n=== TEST 2: seller findByDepartment===");
-		Department dep = new Department(2, null);
-		List<Seller> sellers = sellerDao.findByDepartment(dep);
+		Department depSel = new Department(2, null);
+		List<Seller> sellers = sellerDao.findByDepartment(depSel);
 		for(Seller sel : sellers) {
 			System.out.println(sel);
 		}
@@ -36,7 +36,7 @@ public class Program {
 		}
 		
 		System.out.println("\n=== TEST 4: seller insert===");
-		Seller newSeller = new Seller(null, "James", "james@gmail.com", new Date(), 3500.00, dep);
+		Seller newSeller = new Seller(null, "James", "james@gmail.com", new Date(), 3500.00, depSel);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id: " + newSeller.getId());
 		
@@ -51,5 +51,36 @@ public class Program {
 		int id = sc.nextInt();
 		sellerDao.deleteById(id);
 		System.out.println("Delete completed!");
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("\n=== TEST 1: department findById===");
+		Department dep = departmentDao.findById(3);
+		System.out.println(dep);
+		
+		System.out.println("\n=== TEST 2: department findAll===");
+		List<Department> departments = departmentDao.findAll();
+		for(Department depart : departments){
+		System.out.println(depart);
+		}
+		
+		System.out.println("\n=== TEST 3: department insert===");
+		Department newDep = new Department(null, "Sales");
+		departmentDao.insert(newDep);
+		System.out.println("Inserted! New Id: " + newDep.getId());
+		
+		System.out.println("\n=== TEST 4: department update===");
+		dep = departmentDao.findById(4);
+		dep.setName("Vehicles");
+		departmentDao.update(dep);
+		System.out.println("Update completed!");
+		
+		System.out.println("\n=== TEST 5: department delete===");
+		System.out.println("Enter the Id to delete: ");
+		int idDep = sc.nextInt();
+		departmentDao.deleteById(idDep);
+		System.out.println("Delete completed!");
+		
+		sc.close();
 	}
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import db.Db;
+import model.dao.Impl.DepartmentDaoJDBC;
 import model.dao.Impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(Db.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(Db.getConnection());
 	}
 	
 }
